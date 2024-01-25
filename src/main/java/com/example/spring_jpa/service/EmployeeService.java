10576f2278820c7f3ec5e83330b6d267e0926600
Akshitha Kokkirala <Akshitha.Kokkirala@resmed.com.au>
2024-01-25 19:32:08 +0530

    first-commit

---
+++ src/main/java/com/example/spring_jpa/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.example.spring_jpa.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.spring_jpa.model.Employee;
+
+public interface EmployeeService {
+	
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+	
+}
+
+
+
